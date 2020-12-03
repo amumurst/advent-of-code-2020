a464d32288a45c29dd39e4e995ae32ac357f937f
amumurst <amund.murstad@finn.no>
2020-12-03 06:28:45 +0100

    Solve day3

---
+++ src/main/scala/Day3.scala
@@ -0,0 +1,25 @@
+package no.amumurst
+
+object Day3 extends DayRunner {
+  def slopeCheck(file: List[String], xSkip: Int, ySkip: Int): Int =
+    file
+      .grouped(ySkip)
+      .flatMap(_.headOption)
+      .foldLeft((0, 0)) {
+        case ((pos, n), line) =>
+          ((pos + xSkip) % line.length, n + line.drop(pos).headOption.count(_ == '#'))
+      }
+      ._2
+
+  override def runA(file: List[String]): Any =
+    slopeCheck(file, 3, 1)
+
+  override def runB(file: List[String]): Any =
+    slopeCheck(file, 1, 1).toLong *
+      slopeCheck(file, 3, 1).toLong *
+      slopeCheck(file, 5, 1).toLong *
+      slopeCheck(file, 7, 1).toLong *
+      slopeCheck(file, 1, 2).toLong
+
+  override def isForDay(d: DayTask): Boolean = d.day == 3
+}
--- src/main/scala/Days.scala
+++ src/main/scala/Days.scala
@@ -3,7 +3,8 @@ package no.amumurst
 object Days {
   val allDays: List[DayRunner] = List(
     Day1,
-    Day2
+    Day2,
+    Day3
   )
 
   def runDays(task: DayTask): Unit =
