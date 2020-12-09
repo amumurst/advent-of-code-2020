6e6f84e2a0c4b2c1e57c2958cd42a2fdd13ea950
amumurst <amund.murstad@finn.no>
2020-12-09 10:18:41 +0100

    Slight 9 cleanups

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -3,40 +3,36 @@ package no.amumurst
 import scala.annotation.tailrec
 
 object Day9 extends DayRunner(9) {
-  implicit class Combs(ls: Seq[Long]) {
-    def canSumTo(i: Long): Boolean = ls.flatMap(x => ls.map(_ + x)).contains(i)
-  }
+  def sums(s: LazyList[Long]): LazyList[Long] = s.flatMap(x => s.map(_ + x))
 
   def findWeakness(file: LazyList[Long], preamb: Int): Option[Long] =
     file
       .sliding(preamb + 1)
       .drop(preamb)
-      .find(a => !a.take(preamb).canSumTo(a.last))
+      .find(a => !sums(a.take(preamb)).contains(a.last))
       .flatMap(_.lastOption)
 
-  //258585477
+  @tailrec
+  def findSummable(all: LazyList[Long], searchFor: Long, curr: List[Long] = Nil, s: Long = 0): List[Long] =
+    if (s == searchFor) curr
+    else
+      all.headOption match {
+        case Some(head) => findSummable(all.tail, searchFor, head +: curr, s + head)
+        case None       => Nil
+      }
+
   override def runA(file: List[String]): Any =
     findWeakness(LazyList.from(file.map(_.toLong)), 25)
 
-  //36981213
   override def runB(file: List[String]): Any = {
-    val longs    = LazyList.from(file.map(_.toLong))
-    val weakness = runA(file).asInstanceOf[Option[Long]].get
+    val input = LazyList.from(file.map(_.toLong))
 
-    @tailrec
-    def loop(all: LazyList[Long], curr: List[Long] = Nil, s: Long = 0): List[Long] =
-      if (s == weakness) curr
-      else
-        all.headOption match {
-          case Some(head) => loop(all.tail, head +: curr, s + head)
-          case None       => Nil
-        }
-
-    (0 to file.size)
-      .pipe(LazyList.from[Int])
-      .map(i => longs.drop(i))
-      .map(loop(_))
+    val weakness = findWeakness(input, 25).get
+    LazyList
+      .from[Int](0 to file.size)
+      .map(input.drop)
+      .map(findSummable(_, weakness))
       .find(_.nonEmpty)
-      .map(_.pipe(p => p.max + p.min))
+      .map(p => p.max + p.min)
   }
 }
